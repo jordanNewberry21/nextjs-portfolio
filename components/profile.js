3b6ec8f54606a29e8bd0e76e10b7e23cc0334bae
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-02 10:54:01 -0500

    making a nextjs blog / portfolio

---
+++ components/profile.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+const Profile = () => (
+    <Image 
+        src="/images/profile.jpg"
+        height={220}
+        width={175}
+        alt="Jordan Newberry"
+    />
+)
+
+export default Profile;
